a349315b8f0be5b5f2e5e32eb8de0c223365c1bc
Filipe Carneiro <filipemanuelcarneiro@gmail.com>
2023-03-20 14:24:25 -0100

    Testing

---
+++ Node.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+
+console.log(chalk.blue('Hello Cláudia!'));
+console.log(chalk.green('Hello José!'));
+console.log(chalk.pink('Hello Ricardo!'));
+console.log(chalk.red('Hello Duarte!'));
